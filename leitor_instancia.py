416d1b1f8b5475a2dd436651abb7c97a6490aa44
marcobonelli <marco.bonelli.jr@gmail.com>
2018-07-11 12:59:11 -0300

    update - arquivo de leitura de dados
    finalização das funções de leitura e retorno dos dados macros da mina, bem como função que retorna dados dos blocos pertencentes ao upit

--- leitor_instancia.py
+++ leitor_instancia.py
@@ -1,11 +1,20 @@
 #coding: utf-8
 
+# realiza a leitura do arquivo de blocos e retorna todos os dados, em formato de lista
 def ler_blocos():
 
 	database = [line.rstrip('\n') for line in open('newman1.blocks')]
+	
+	for i in range(0, len(database)):
+		database[i] = database[i].split(' ')
+		for j in [0, 1, 2, 3, 10]:
+			database[i][j] = int(database[i][j])
+		for j in [5, 6, 7, 8, 9]:
+			database[i][j] = float(database[i][j])
 
-	return 0
+	return database
 
+# realiza a leitura do arquivo contendo o upit e retorna somente os blocos pertencentes ao upit, em formato de lista
 def ler_upit():
 
 	database = [line.rstrip('\n') for line in open('newman1.upit')]
@@ -18,10 +27,26 @@ def ler_upit():
 
 	return upit
 
+# realiza a leitura do arquivo de precedencia de blocos e retorna todos os dados, em formato de lista
 def ler_precedencia():
 
 	database = [line.rstrip('\n') for line in open('newman1.prec')]
 
-	return 0
+	for i in range(0, len(database)):
+		database[i] = database[i].split(' ')
+		for j in range(0, len(database[i])):
+			database[i][j] = int(database[i][j])
 
-ler_upit()
+	return database
+
+# retorna todos os dados referentes ao blocos que pertencem ao upit, em formato de lista
+def gerar_blocos_upit():
+
+	blocos = ler_blocos()
+	upit = ler_upit()
+
+	database = []
+	for i in upit:
+		database.append(blocos[i])
+
+	return database
