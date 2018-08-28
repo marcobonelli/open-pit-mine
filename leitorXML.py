2f1753952b6d82b45b2add2571a576b397ba9bb1
thiagoSilvaGit <thiagoep@gmail.com>
2018-08-27 22:47:46 -0300

    devolvendo o leitorXML.py

---
+++ leitorXML.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import xmlschema
+from pprint import pprint
+
+
+def LerXML(arq):
+    xs = xmlschema.XMLSchema('instancia.xsd')
+    print xs.is_valid(arq)
+    pprint(xs.to_dict(arq))
+    my_dict = xs.to_dict(arq)
+    return my_dict
+
